af8c4212909e528e5a299baad30eca137719dbca
akash <akasky70@gmail.com>
2020-10-02 22:38:47 +0545

    Feat: add ts templates and create scafoldings in module 🔥🔥🔥

--- src/react-feature.ts
+++ src/react-feature.ts
@@ -1,7 +1,13 @@
 import ReactFeatureCli = require('.');
-import { createDir, createFile } from './helper/file.helper';
+
+import { typeTemplate } from './template/types';
+import { styleTemplate } from './template/style';
+import { routeTemplate } from './template/route';
+import { featureTemplate } from './template/feature';
+import { contextTypescript } from './template/context';
 
 import { Flag } from './react-feature.type';
+import { createDir, createFile } from './helper/file.helper';
 
 export const createScaffold = (
   self: ReactFeatureCli,
@@ -12,16 +18,24 @@ export const createScaffold = (
     self.log('Typescript selected');
 
     const featureNameStr = `/${featureName}`;
-
     const dirPath = createDir(featureNameStr);
 
     if (dirPath) {
-      const fileName = `${dirPath}/${featureName}.context.tsx`;
-      createFile(fileName, 'This is the content');
+      const featureFile = `${dirPath}/${featureName}.tsx`;
+      const typeFile = `${dirPath}/${featureName}.type.ts`;
+      const styleFile = `${dirPath}/${featureName}.style.ts`;
+      const routeFile = `${dirPath}/${featureName}.route.tsx`;
+      const contextFile = `${dirPath}/${featureName}.context.tsx`;
+
+      createFile(styleFile, styleTemplate());
+      createFile(typeFile, typeTemplate(featureName));
+      createFile(routeFile, routeTemplate(featureName));
+      createFile(featureFile, featureTemplate(featureName));
+      createFile(contextFile, contextTypescript(featureName));
+
+      self.log(featureName, ' module created');
     }
   }
 
-  self.log('Feature name is', featureName);
-
-  // create js scafolding
+  //Todo: create js scafolding
 };
